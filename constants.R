8a394525520153f8fef038b9ca4e7222456ba0f0
brynmwangy <brianmwangi849@gmail.com>
2022-01-09 21:22:14 +0300

    Data update

--- constants.R
+++ constants.R
@@ -1,5 +1,6 @@
 #reading app data
 app_df<-readRDS("./www/app_data.rds")
 
+
 vernacular_name <- unique(app_df$table_data$vernacularName)
 scientific_name <- unique(app_df$table_data$scientificName)
